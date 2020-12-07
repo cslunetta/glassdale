42bb6800d2b1d408a981933a9713b9683f41b8ae
cslunetta <cslunetta@outlook.com>
2020-12-07 11:20:00 -0600

    made more readable

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -6,23 +6,23 @@ import { useConvictions } from "../convictions/convictionProvider.js";
 const eventHub = document.querySelector(".container");
 const criminalElement = document.querySelector(".criminalsContainer");
 
-//listen for custom event on convictionSelect.js of "crimeThatWasChosen"
-const render = (criminals) => {
-  let criminalCards = [];
-  for (const perp of criminals) {
-    criminalCards.push(criminal(perp));
-  }
-  criminalElement.innerHTML = criminalCards.join("");
+//Render ALL criminals initally
+export const criminalList = () => {
+  getCriminals().then(() => {
+    let perps = useCriminals();
+    render(perps);
+  });
 };
 
+//listen for custom event on convictionSelect.js of "crimeThatWasChosen"
 eventHub.addEventListener("crimeChosen", (event) => {
   if (event.detail.crimeThatWasChosen !== "0") {
     //filter criminals by crime committed
     const crimes = useConvictions();
     const crime = crimes.find(
       (crime) => crime.id === parseInt(event.detail.crimeThatWasChosen)
-      );
-      
+    );
+
     const criminals = useCriminals();
     const matchingCriminals = criminals.filter(
       (criminal) => criminal.conviction === crime.name
@@ -32,10 +32,10 @@ eventHub.addEventListener("crimeChosen", (event) => {
   }
 });
 
-//Render ALL criminals initally
-export const criminalList = () => {
-  getCriminals().then(() => {
-    let perps = useCriminals();
-    render(perps);
-  });
+const render = (criminals) => {
+  let criminalCards = [];
+  for (const perp of criminals) {
+    criminalCards.push(criminal(perp));
+  }
+  criminalElement.innerHTML = criminalCards.join("");
 };
