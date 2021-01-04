6fc20b6e87e58f8d4a9482fdba2f1a1d52d165a8
cslunetta <cslunetta@outlook.com>
2021-01-04 13:21:17 -0600

    updated classes and modularized css files

--- scripts/criminals/associateAlibis/alibi.js
+++ scripts/criminals/associateAlibis/alibi.js
@@ -1,6 +1,8 @@
 export const alibiHTML = (alibi) => {
   return `
-    <h2>Known Associates:${alibi.name}</h2>
-    <div>Alibi: ${alibi.alibi}</div>
+    <section class="alibiCard">
+      <h2>Known Associate: ${alibi.name}</h2>
+      <div>Alibi: ${alibi.alibi}</div>
+    </section>
   `;
 };
